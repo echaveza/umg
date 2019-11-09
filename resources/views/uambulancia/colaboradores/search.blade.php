899c765c69bc7f57b79c84690f03cd0ef375a626
echaveza <edychavez4175@gmail.com>
2019-11-09 05:45:39 -0600

    version 3

--- resources/views/uambulancia/colaboradores/search.blade.php
+++
@@ -1,9 +0,0 @@
-{!! Form::open(array('url'=>'combustible/colaboradores','method'=>'GET','autocomplete'=>'off','role'=>'search')) !!}
-
-<body>
-	{!! Form::open(array('url'=>'combustible/colaboradores','method'=>'GET','autocomplete'=>'off','role'=>'search')) !!}
-<form>
-  <input type="text" name="searchText" name="search" placeholder="Search..">
-</form>
-{{Form::close()}}
-</body>
--- resources/views/uambulancia/vehiculo/search.blade.php
+++
@@ -1,7 +0,0 @@
-<body>
-	{!! Form::open(array('url'=>'combustible/vehiculo','method'=>'GET','autocomplete'=>'off','role'=>'search')) !!}
-<form>
-  <input type="text" name="searchText" name="search" placeholder="Search..">
-</form>
-{{Form::close()}}
-</body>
